d4bb509ededbfed77b200aaa257dc335187248e8
p-lucero <p-lucero@users.noreply.github.com>
2019-02-09 13:17:22 -0700

    Get listeners working, dummy functions

--- background.js
+++ background.js
@@ -1,1 +1,20 @@
-console.log("alksjdfa;lksjdf;asj")
+function onTabCreation(tab){
+	console.log('tab creation event fired')
+}
+
+function onTabUpdate(tab){
+	console.log('tab update event fired')
+}
+
+function onTabRemoval(tab){
+	console.log('tab removal event fired')
+}
+
+function onTabFocusChange(tab){
+	console.log('tab focus change event fired')
+}
+
+browser.tabs.onCreated.addListener(onTabCreation)
+browser.tabs.onUpdated.addListener(onTabUpdate)
+browser.tabs.onRemoved.addListener(onTabRemoval)
+browser.tabs.onHighlighted.addListener(onTabFocusChange)
