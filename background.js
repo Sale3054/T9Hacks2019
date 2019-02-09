caf097f67b5f3990c1fb5e83dc47deffc1dc1364
p-lucero <p-lucero@users.noreply.github.com>
2019-02-09 14:36:18 -0700

    Data collection

--- background.js
+++ background.js
@@ -1,25 +1,86 @@
-function onTabCreation(tab){
-	console.log('tab creation event fired')
+var active_url = undefined
+var active_timestamp = Date.now()
+
+var time_intervals = {} // TODO add persistence between browser sessions
+var total_times = {}
+
+function startNewTimeInterval(url)
+{
+	if (url !== "null")
+	{
+		console.log('Starting a new time interval for ' + url)
+		active_url = url
+		active_timestamp = Date.now()	
+	}
 }
 
-function onTabUpdate(tab){
-	console.log('tab update event fired')
+function stopCurrentTimeInterval()
+{
+	if (active_url !== undefined)
+	{
+		console.log('Stopping the time interval for ' + active_url)
+		var end_timestamp = Date.now()
+		if (time_intervals[active_url] === undefined)
+		{
+			time_intervals[active_url] = []
+		}
+		time_intervals[active_url].push([active_timestamp, end_timestamp])
+		if (total_times[active_url] === undefined)
+		{
+			total_times[active_url] = end_timestamp - active_timestamp
+		}
+		else
+		{
+			total_times[active_url] += end_timestamp - active_timestamp		
+		}
+		active_url = undefined
+	}
 }
 
-function onTabRemoval(tab){
-	console.log('tab removal event fired')
+function onTabCreation(tab)
+{
+	// don't need to handle tab creation because
+	// if the tab is created in the foreground, this will imply a focus change event
+	// if the tab is created in the background, then the time spent does not need to be tracked
+	// console.log('tab creation event fired')
 }
 
-function onTabFocusChange(tab){
-	console.log('tab focus change event fired')
+function onTabUpdate(tabID, changeInfo, tab)
+{
+	console.log('Tab update event fired')
+	var tab_url = getURL(tab)
+	if (tab.status === "complete" && tab_url !== active_url && tab.active)
+	{
+		stopCurrentTimeInterval()
+		startNewTimeInterval(tab_url)
+	}
 }
 
-function getURL(tab){
+function onTabRemoval(tabID, removeInfo)
+{
+	stopCurrentTimeInterval()
+	active_url = undefined
+}
+
+function onTabHighlight(highlightInfo)
+{
+	// don't need to handle tab focus change because tab focus change implies tab update
+}
+
+function getURL(tab)
+{
 	var current_url = tab.url
-	var url_origin = new URL(current_url).origin
-	return url_origin
+	try
+	{
+		return (new URL(current_url)).origin.replace(new RegExp('https?://'), '')
+	}
+	catch(err)
+	{
+		return undefined
+	}
 }
+
 browser.tabs.onCreated.addListener(onTabCreation)
 browser.tabs.onUpdated.addListener(onTabUpdate)
 browser.tabs.onRemoved.addListener(onTabRemoval)
-browser.tabs.onHighlighted.addListener(onTabFocusChange)
+browser.tabs.onHighlighted.addListener(onTabHighlight)
