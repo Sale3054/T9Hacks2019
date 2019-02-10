0b313983e830a8a4f61f9b758cc8ed09625009b4
SALeon <sale3054@colorado.edu>
2019-02-09 19:13:28 -0700

    Added git ignore, Chart.js dependency, Corrected bullshit Summarize.js,and fixed issues w/ Summarize.html (working)

--- content_scripts/summarize.js
+++ content_scripts/summarize.js
@@ -1,15 +1,1 @@
-function handleResponse(message) {
-  console.log(`Message from the background script:  ${message.response}`);
-}
-
-function handleError(error) {
-  console.log(`Error: ${error}`);
-}
-
-
-function requestDay(e, time)
-{
-	var sending = browser.runtime.sendMessage({ type : 'day'});
-	sending.then(handleResponse, handleError);
-}
-window.addEventListener("click", notifyBackgroundPage);
+//I Don't do anything.
