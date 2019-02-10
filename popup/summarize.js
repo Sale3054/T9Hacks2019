ded8d0f52d1f9d46bca9aefb32330087bdf29cf2
SALeon <sale3054@colorado.edu>
2019-02-09 19:02:02 -0700

    Working request buttons.

--- popup/summarize.js
+++ popup/summarize.js
@@ -4,6 +4,7 @@
  */
 function listenForClicks() {
   document.addEventListener("click", (e) => {
+  	requestDate(e);
 
     /**
      * Given the name of a beast, get the URL to the corresponding image.
@@ -55,7 +56,8 @@ function listenForClicks() {
  * There was an error executing the script.
  * Display the popup's error message, and hide the normal UI.
  */
-function reportExecuteScriptError(error) {
+function reportExecuteScriptError(error)
+{
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
   console.error(`Failed to execute content script: ${error.message}`);
@@ -65,5 +67,5 @@ function reportExecuteScriptError(error) {
  * When the popup loads, inject a content script into the active tab,
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
- */
-document.addEventListener("click", listenForClicks).then(handleError).catch(handleError);
+ **/
+listenForClicks();
